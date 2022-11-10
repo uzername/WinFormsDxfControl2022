bd50ecf60805ca09e78a2b21ae1eab2e91c03d35
IVANP <IVANP@PC-IPO2>
2022-11-10 17:06:47 +0200

    Add project files.

---
+++ WinFormsDxfControl/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WinFormsDxfControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WinFormsDxfRendererInstance = new WinFormsDxfControl1.WinFormsDxfRenderer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.checkBoxMirror = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WinFormsDxfRendererInstance
+            // 
+            this.WinFormsDxfRendererInstance.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WinFormsDxfRendererInstance.Location = new System.Drawing.Point(0, 0);
+            this.WinFormsDxfRendererInstance.Name = "WinFormsDxfRendererInstance";
+            this.WinFormsDxfRendererInstance.Size = new System.Drawing.Size(797, 451);
+            this.WinFormsDxfRendererInstance.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(90, 454);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(659, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelPath
+            // 
+            this.labelPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(0, 457);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(84, 15);
+            this.labelPath.TabIndex = 2;
+            this.labelPath.Text = "Path to dxf file";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(755, 454);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(42, 23);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMirror
+            // 
+            this.checkBoxMirror.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxMirror.AutoSize = true;
+            this.checkBoxMirror.Location = new System.Drawing.Point(3, 481);
+            this.checkBoxMirror.Name = "checkBoxMirror";
+            this.checkBoxMirror.Size = new System.Drawing.Size(59, 19);
+            this.checkBoxMirror.TabIndex = 4;
+            this.checkBoxMirror.Text = "Mirror";
+            this.checkBoxMirror.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Angle, from 0 to 360 deg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxMirror);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.WinFormsDxfRendererInstance);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WinFormsDxfControl1.WinFormsDxfRenderer WinFormsDxfRendererInstance;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.CheckBox checkBoxMirror;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormsDxfControl1/WinFormsDxfRenderer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IxMilia.Dxf;
+
+namespace WinFormsDxfControl1
+{
+    /// <summary>
+    /// here is control that draws DXF file, rotated, mirrored and scaled.
+    /// this control has only drawing pane.
+    /// </summary>
+    public partial class WinFormsDxfRenderer : UserControl
+    {
+        private DxfFile dxfFile;
+        /// <summary>
+        /// list of figures to render. The set contains figures already slammed to zero point
+        /// also it is already rotated
+        /// </summary>
+        private List<RenderFigure> AllFigures = new List<RenderFigure>();
+        class RenderFigure
+        {
+            public Color usedColor;
+        }
+        /// <summary>
+        /// position of rendered line
+        /// </summary>
+        class RenderLine : RenderFigure
+        {
+            public double StartX;
+            public double EndX;
+            public double StartY;
+            public double EndY;
+        }
+        /// <summary>
+        /// position of rendered arc as required by .NET
+        /// </summary>
+        class RenderArc : RenderFigure
+        {
+            /// <summary>
+            /// The x-coordinate of the upper-left corner of the rectangle that defines the ellipse.
+            /// </summary>
+            public double x;
+            /// <summary>
+            /// The y-coordinate of the upper-left corner of the rectangle that defines the ellipse.
+            /// </summary>
+            public double y;
+            /// <summary>
+            /// Width of the rectangle that defines the ellipse.
+            /// </summary>
+            public double width;
+            /// <summary>
+            /// Height of the rectangle that defines the ellipse.
+            /// </summary>
+            public double height;
+            /// <summary>
+            /// Angle in degrees measured clockwise from the x-axis to the starting point of the arc.
+            /// </summary>
+            public double startAngle;
+            /// <summary>
+            /// Angle in degrees measured clockwise from the startAngle parameter to ending point of the arc.
+            /// </summary>
+            public double sweepAngle;
+        }
+
+        public WinFormsDxfRenderer()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// entry point to control. Here it begins.
+        /// </summary>
+        /// <param name="reparseRequired">read dxf file from scratch using ixmilia</param>
+        /// <param name="inPathToFile">path to file</param>
+        /// <param name="rotationAngleDeg">angle in degrees for rotation</param>
+        /// <param name="mirror">do we apply mirror before rotation?</param>
+        public void processDXFfileNow(bool reparseRequired, string inPathToFile, double rotationAngleDeg, bool mirror)
+        {
+            if (reparseRequired)
+            {
+                dxfFile = DxfFile.Load(inPathToFile);
+            }
+        }
+        private void WinFormsDxfRenderer_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+        }
+    }
+}
