988bf8acf76dbcdc2f94da3428bffc7aaa8c2af1
uzername <ivanpost777@gmail.com>
2022-11-14 19:02:18 +0200

    IvanP. A lot of changes

--- WinFormsDxfControl/Form1.Designer.cs
+++ WinFormsDxfControl/Form1.Designer.cs
@@ -35,6 +35,8 @@ namespace WinFormsDxfControl
             this.buttonBrowse = new System.Windows.Forms.Button();
             this.checkBoxMirror = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // WinFormsDxfRendererInstance
@@ -42,16 +44,17 @@ namespace WinFormsDxfControl
             this.WinFormsDxfRendererInstance.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.WinFormsDxfRendererInstance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.WinFormsDxfRendererInstance.Location = new System.Drawing.Point(0, 0);
             this.WinFormsDxfRendererInstance.Name = "WinFormsDxfRendererInstance";
-            this.WinFormsDxfRendererInstance.Size = new System.Drawing.Size(797, 451);
+            this.WinFormsDxfRendererInstance.Size = new System.Drawing.Size(797, 461);
             this.WinFormsDxfRendererInstance.TabIndex = 0;
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(90, 454);
+            this.textBox1.Location = new System.Drawing.Point(90, 464);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(659, 23);
             this.textBox1.TabIndex = 1;
@@ -60,7 +63,7 @@ namespace WinFormsDxfControl
             // 
             this.labelPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.labelPath.AutoSize = true;
-            this.labelPath.Location = new System.Drawing.Point(0, 457);
+            this.labelPath.Location = new System.Drawing.Point(0, 467);
             this.labelPath.Name = "labelPath";
             this.labelPath.Size = new System.Drawing.Size(84, 15);
             this.labelPath.TabIndex = 2;
@@ -68,7 +71,8 @@ namespace WinFormsDxfControl
             // 
             // buttonBrowse
             // 
-            this.buttonBrowse.Location = new System.Drawing.Point(755, 454);
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonBrowse.Location = new System.Drawing.Point(755, 464);
             this.buttonBrowse.Name = "buttonBrowse";
             this.buttonBrowse.Size = new System.Drawing.Size(42, 23);
             this.buttonBrowse.TabIndex = 3;
@@ -79,7 +83,7 @@ namespace WinFormsDxfControl
             // 
             this.checkBoxMirror.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBoxMirror.AutoSize = true;
-            this.checkBoxMirror.Location = new System.Drawing.Point(3, 481);
+            this.checkBoxMirror.Location = new System.Drawing.Point(3, 491);
             this.checkBoxMirror.Name = "checkBoxMirror";
             this.checkBoxMirror.Size = new System.Drawing.Size(59, 19);
             this.checkBoxMirror.TabIndex = 4;
@@ -90,17 +94,31 @@ namespace WinFormsDxfControl
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 482);
+            this.label1.Location = new System.Drawing.Point(68, 492);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(137, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "Angle, from 0 to 360 deg";
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDown1.Location = new System.Drawing.Point(211, 489);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 23);
+            this.numericUpDown1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.ClientSize = new System.Drawing.Size(800, 513);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBoxMirror);
             this.Controls.Add(this.buttonBrowse);
@@ -109,6 +127,7 @@ namespace WinFormsDxfControl
             this.Controls.Add(this.WinFormsDxfRendererInstance);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +141,7 @@ namespace WinFormsDxfControl
         private System.Windows.Forms.Button buttonBrowse;
         private System.Windows.Forms.CheckBox checkBoxMirror;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- WinFormsDxfControl1/WinFormsDxfRenderer.cs
+++ WinFormsDxfControl1/WinFormsDxfRenderer.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using IxMilia.Dxf;
+using IxMilia.Dxf.Entities;
 
 namespace WinFormsDxfControl1
 {
@@ -16,13 +17,42 @@ namespace WinFormsDxfControl1
     /// this control has only drawing pane.
     /// </summary>
     public partial class WinFormsDxfRenderer : UserControl
-    {
-        private DxfFile dxfFile;
-        /// <summary>
-        /// list of figures to render. The set contains figures already slammed to zero point
-        /// also it is already rotated
-        /// </summary>
-        private List<RenderFigure> AllFigures = new List<RenderFigure>();
+    {        
+        //====== INTERNAL CLASSES ==========
+        class BoundBox
+        {
+            public double upperLeftX = 0;
+            public double upperLeftY = 0;
+            public double bottomRightX = 0;
+            public double bottomRightY = 0;
+            public BoundBox()
+            {
+                upperLeftX = 0; upperLeftY = 0;
+                bottomRightX = 0; bottomRightY = 0;
+            }
+        }
+        
+        class RenderStruct
+        {
+            /// <summary>
+            /// list of figures to render. it is flipped. And prepared to be rendered. I mean, rotated and scaled to fit window
+            /// </summary>
+            public List<RenderFigure> AllFigures = new List<RenderFigure>();
+            /// <summary>
+            /// the raw bound box of entries in file. Should not change often. Parsed upon filling the AllFigures list            
+            /// </summary>
+            public BoundBox currentRawBoundBox = new BoundBox();
+            /// <summary>
+            /// Bound Box with rotation applied
+            /// </summary>
+            public BoundBox currentRotationBoundBox = new BoundBox();
+            /// <summary>
+            /// current scale factor to fit into window
+            /// </summary>
+            public double uniformScaleFactor = 1.0;
+            public double offsetX = 0.0;
+            public double offsetY = 0.0;
+        }
         class RenderFigure
         {
             public Color usedColor;
@@ -67,15 +97,299 @@ namespace WinFormsDxfControl1
             /// </summary>
             public double sweepAngle;
         }
-
+        //====== INTERNAL variables ==========
+        // names of ignored layers. should be lowercase, lol
+        private List<String> ignoredLayers = new List<string> { "dxffix", "ext prof" };
+        /// <summary>
+        /// current entries of dxf file 
+        /// </summary>
+        private DxfFile dxfFile;
+        /// <summary>
+        /// ALL render-related data
+        /// </summary>
+        RenderStruct renderStruct;
         public WinFormsDxfRenderer()
         {
             InitializeComponent();
         }
+        private double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+
+        // I want to align dxf profile somehow to axes. I find out bounding box of dxf file
+        // I also use here values for rotation
+        // inRotationAngleRad is from 0 to 2PI . It does not matter though
+        /* https://stackoverflow.com/questions/2259476/rotating-a-point-about-another-point-2d
+         * If you rotate point (px, py) around point (ox, oy) by angle theta you'll get:
+            p'x = cos(theta) * (px-ox) - sin(theta) * (py-oy) + ox
+            p'y = sin(theta) * (px-ox) + cos(theta) * (py-oy) + oy
+         */
+        private BoundBox getBoundBoxOfDxf(DxfFile inObtainedStructure, bool calculateRotation, double inRotationCenterX, double inRotationCenterY, double inRotationAngleRad)
+        {
+            bool isFirstEstimation = true;
+            bool gotInside = false;
+            // bounding box total
+            BoundBox retStruct = new BoundBox();
+            BoundBox currentStruct = new BoundBox();
+            void handleLineDimensions(DxfLine lineEntity)
+            {
+                double lineEntityP1X = lineEntity.P1.X;
+                double lineEntityP2X = lineEntity.P2.X;
+                double lineEntityP1Y = lineEntity.P1.Y;
+                double lineEntityP2Y = lineEntity.P2.Y;
+                if (calculateRotation)
+                {
+                    double lineEntityP1XNew = Math.Cos(inRotationAngleRad) * (lineEntityP1X - inRotationCenterX) - Math.Sin(inRotationAngleRad) * (lineEntityP1Y - inRotationCenterY) + inRotationCenterX;
+                    double lineEntityP1YNew = Math.Sin(inRotationAngleRad) * (lineEntityP1X - inRotationCenterX) + Math.Cos(inRotationAngleRad) * (lineEntityP1Y - inRotationCenterY) + inRotationCenterY;
+                    double lineEntityP2XNew = Math.Cos(inRotationAngleRad) * (lineEntityP2X - inRotationCenterX) - Math.Sin(inRotationAngleRad) * (lineEntityP2Y - inRotationCenterY) + inRotationCenterX;
+                    double lineEntityP2YNew = Math.Sin(inRotationAngleRad) * (lineEntityP2X - inRotationCenterX) + Math.Cos(inRotationAngleRad) * (lineEntityP2Y - inRotationCenterY) + inRotationCenterY;
+                    lineEntityP1X = lineEntityP1XNew;
+                    lineEntityP2X = lineEntityP2XNew;
+                    lineEntityP1Y = lineEntityP1YNew;
+                    lineEntityP2Y = lineEntityP2YNew;
+                }
+                currentStruct.bottomRightX = Math.Max(lineEntityP1X, lineEntityP2X);
+                currentStruct.bottomRightY = Math.Min(lineEntityP1Y, lineEntityP2Y);
+                currentStruct.upperLeftX = Math.Min(lineEntityP1X, lineEntityP2X);
+                currentStruct.upperLeftY = Math.Max(lineEntityP1Y, lineEntityP2Y);
+            }
+            void handleArcDimensions(DxfArc arc)
+            {
+                double arcCenterX = arc.Center.X;
+                double arcCenterY = arc.Center.Y;
+                //estimation of arc bound box is going to be fun
+                double radAngleStart = ConvertDegreesToRadians(arc.StartAngle);
+                double radAngleEnd = ConvertDegreesToRadians(arc.EndAngle);
+                if (radAngleStart > radAngleEnd)
+                {
+                    radAngleEnd += Math.PI * 2;
+                }
+                if (calculateRotation)
+                {
+                    double arcCenterXNew = Math.Cos(inRotationAngleRad) * (arcCenterX - inRotationCenterX) - Math.Sin(inRotationAngleRad) * (arcCenterY - inRotationCenterY) + inRotationCenterX;
+                    double arcCenterYNew = Math.Sin(inRotationAngleRad) * (arcCenterX - inRotationCenterX) + Math.Cos(inRotationAngleRad) * (arcCenterY - inRotationCenterY) + inRotationCenterY;
+                    radAngleEnd += inRotationAngleRad;
+                    radAngleStart += inRotationAngleRad;
+                    arcCenterX = arcCenterXNew;
+                    arcCenterY = arcCenterYNew;
+                }
+                double startPointXcoordinate = arcCenterX + Math.Cos(radAngleStart) * arc.Radius;
+                double startPointYcoordinate = arcCenterY + Math.Sin(radAngleStart) * arc.Radius;
+                double endPointXcoordinate = arcCenterX + Math.Cos(radAngleEnd) * arc.Radius;
+                double endPointYcoordinate = arcCenterY + Math.Sin(radAngleEnd) * arc.Radius;
+
+                currentStruct.bottomRightX = Math.Max(startPointXcoordinate, endPointXcoordinate);
+                currentStruct.bottomRightY = Math.Min(startPointYcoordinate, endPointYcoordinate);
+                currentStruct.upperLeftX = Math.Min(startPointXcoordinate, endPointXcoordinate);
+                currentStruct.upperLeftY = Math.Max(startPointYcoordinate, endPointYcoordinate);
+                double angleCounter = 0;
+                //iterate in steps of pi/2 from 0 to 4*pi
+                while (angleCounter <= Math.PI * 4.0)
+                {
+                    if ((angleCounter >= radAngleStart) && (angleCounter <= radAngleEnd))
+                    {
+                        if ((angleCounter == 0) || (angleCounter == (2 * Math.PI)) || (angleCounter == (4 * Math.PI)))
+                        {
+                            currentStruct.bottomRightX = Math.Max(currentStruct.bottomRightX, arcCenterX + arc.Radius);
+
+                        }
+                        else if ((angleCounter == (3 * Math.PI / 2.0)) || (angleCounter == (11 * Math.PI / 2.0)))
+                        {
+                            currentStruct.bottomRightY = Math.Min(currentStruct.bottomRightY, arcCenterY - arc.Radius);
+                        }
+                        else if ((angleCounter == Math.PI) || (angleCounter == 3 * Math.PI))
+                        {
+                            currentStruct.upperLeftX = Math.Min(currentStruct.upperLeftX, arcCenterX - arc.Radius);
+                        }
+                        else if ((angleCounter == (Math.PI / 2.0)) || (angleCounter == (5 * Math.PI / 2.0)))
+                        {
+                            currentStruct.upperLeftY = Math.Max(currentStruct.upperLeftY, arcCenterY + arc.Radius);
+                        }
+
+
+                    }
+                    if (angleCounter >= radAngleEnd)
+                    {
+                        break;
+                    }
+                    angleCounter += Math.PI / 2.0;
+                }
+            }
+            void handleCircleDimensions(DxfCircle circle)
+            {
+                double circleCenterX = circle.Center.X;
+                double circleCenterY = circle.Center.Y;
+                double circleRadius = circle.Radius;
+                if (calculateRotation)
+                {
+                    double circleCenterXNew = Math.Cos(inRotationAngleRad) * (circleCenterX - inRotationCenterX) - Math.Sin(inRotationAngleRad) * (circleCenterY - inRotationCenterY) + inRotationCenterX;
+                    double circleCenterYNew = Math.Sin(inRotationAngleRad) * (circleCenterX - inRotationCenterX) + Math.Cos(inRotationAngleRad) * (circleCenterY - inRotationCenterY) + inRotationCenterY;
+                    circleCenterX = circleCenterXNew;
+                    circleCenterY = circleCenterYNew;
+                }
+                currentStruct.bottomRightX = circleCenterX + circleRadius;
+                currentStruct.bottomRightY = circleCenterY - circleRadius;
+                currentStruct.upperLeftX = circleCenterX - circleRadius;
+                currentStruct.upperLeftY = circleCenterY + circleRadius;
+            }
+            void handlePolylineDimensions(List<DxfEntity> polylineEntities)
+            {
+                foreach (DxfEntity entity in polylineEntities)
+                {
+                    currentStruct = new BoundBox();
+                    switch (entity.EntityType)
+                    {
+                        case DxfEntityType.Line:
+                            {
+                                if (gotInside == false) { gotInside = true; }
+                                DxfLine lineEntity = entity as DxfLine;
+                                handleLineDimensions(lineEntity);
+                                break;
+                            }
+                        case DxfEntityType.Arc:
+                            {
+                                if (gotInside == false) { gotInside = true; }
+                                DxfArc arc = (DxfArc)entity;
+                                handleArcDimensions(arc);
+                                break;
+                            }
+                        case DxfEntityType.Circle:
+                            {
+                                if (gotInside == false) { gotInside = true; }
+                                DxfCircle circle = (DxfCircle)entity;
+                                handleCircleDimensions(circle);
+                                break;
+                            }
+                        case DxfEntityType.Polyline:
+                        case DxfEntityType.LwPolyline:
+                            {
+                                if (gotInside == false)
+                                {
+                                    gotInside = true;
+                                }
+                                List<DxfEntity> obtainedEntitiesFromPolyline = new List<DxfEntity>();
+                                if (entity is DxfPolyline)
+                                {
+                                    obtainedEntitiesFromPolyline = (entity as DxfPolyline).AsSimpleEntities().ToList();
+                                }
+                                else if (entity is DxfLwPolyline)
+                                {
+                                    obtainedEntitiesFromPolyline = (entity as DxfLwPolyline).AsSimpleEntities().ToList();
+                                }
+
+                                break;
+                            }
+                    }
+                    if (gotInside)
+                    {
+                        if (isFirstEstimation)
+                        {
+                            retStruct.bottomRightX = currentStruct.bottomRightX;
+                            retStruct.bottomRightY = currentStruct.bottomRightY;
+                            retStruct.upperLeftX = currentStruct.upperLeftX;
+                            retStruct.upperLeftY = currentStruct.upperLeftY;
+                            isFirstEstimation = false;
+                        }
+                        else
+                        {
+                            retStruct.bottomRightX = Math.Max(currentStruct.bottomRightX, retStruct.bottomRightX);
+                            retStruct.bottomRightY = Math.Min(currentStruct.bottomRightY, retStruct.bottomRightY);
+                            retStruct.upperLeftX = Math.Min(currentStruct.upperLeftX, retStruct.upperLeftX);
+                            retStruct.upperLeftY = Math.Max(currentStruct.upperLeftY, retStruct.upperLeftY);
+                        }
+                        gotInside = false;
+                    }
+                }
+            }
+            foreach (DxfEntity entity in inObtainedStructure.Entities)
+            {
+                if (ignoredLayers.Contains(entity.Layer.ToLower()) == false)
+                {
+                    currentStruct = new BoundBox();
+                    switch (entity.EntityType)
+                    {
+                        case DxfEntityType.Line:
+                            {
+                                if (gotInside == false)
+                                {
+                                    gotInside = true;
+                                }
+                                DxfLine lineEntity = entity as DxfLine;
+                                handleLineDimensions(lineEntity);
+                                break;
+                            }
+                        case DxfEntityType.Arc:
+                            {
+                                if (gotInside == false)
+                                {
+                                    gotInside = true;
+                                }
+                                DxfArc arc = (DxfArc)entity;
+
+                                handleArcDimensions(arc);
+
+                                break;
+                            }
+                        case DxfEntityType.Circle:
+                            {
+                                if (gotInside == false)
+                                {
+                                    gotInside = true;
+                                }
+                                DxfCircle circle = (DxfCircle)entity;
+                                handleCircleDimensions(circle);
+                                break;
+                            }
+                        case DxfEntityType.Polyline:
+                        case DxfEntityType.LwPolyline:
+                            {
+                                if (gotInside == false)
+                                {
+                                    gotInside = true;
+                                }
+                                List<DxfEntity> obtainedEntitiesFromPolyline = new List<DxfEntity>();
+                                if (entity is DxfPolyline)
+                                {
+                                    obtainedEntitiesFromPolyline = (entity as DxfPolyline).AsSimpleEntities().ToList();
+                                }
+                                else if (entity is DxfLwPolyline)
+                                {
+                                    obtainedEntitiesFromPolyline = (entity as DxfLwPolyline).AsSimpleEntities().ToList();
+                                }
+                                handlePolylineDimensions(obtainedEntitiesFromPolyline);
+                                break;
+                            }
+                    }
+                    if (gotInside)
+                    {
+                        if (isFirstEstimation)
+                        {
+                            retStruct.bottomRightX = currentStruct.bottomRightX;
+                            retStruct.bottomRightY = currentStruct.bottomRightY;
+                            retStruct.upperLeftX = currentStruct.upperLeftX;
+                            retStruct.upperLeftY = currentStruct.upperLeftY;
+                            isFirstEstimation = false;
+                        }
+                        else
+                        {
+                            retStruct.bottomRightX = Math.Max(currentStruct.bottomRightX, retStruct.bottomRightX);
+                            retStruct.bottomRightY = Math.Min(currentStruct.bottomRightY, retStruct.bottomRightY);
+                            retStruct.upperLeftX = Math.Min(currentStruct.upperLeftX, retStruct.upperLeftX);
+                            retStruct.upperLeftY = Math.Max(currentStruct.upperLeftY, retStruct.upperLeftY);
+                        }
+                        gotInside = false;
+                    }
+                }
+            }
+            return retStruct;
+        }
+
         /// <summary>
-        /// entry point to control. Here it begins.
+        /// entry point to control. Here it begins. Process DXF file. Or re-calculate parameters of figures to render
         /// </summary>
-        /// <param name="reparseRequired">read dxf file from scratch using ixmilia</param>
+        /// <param name="reparseRequired">read dxf file from scratch using ixmilia. RotateRequired and RescaleRequired parameters are ignored if this one is true</param>
         /// <param name="inPathToFile">path to file</param>
         /// <param name="rotationAngleDeg">angle in degrees for rotation</param>
         /// <param name="mirror">do we apply mirror before rotation?</param>
@@ -84,7 +398,16 @@ namespace WinFormsDxfControl1
             if (reparseRequired)
             {
                 dxfFile = DxfFile.Load(inPathToFile);
+                // create renderstruct
+                renderStruct = new RenderStruct();
+                renderStruct.currentRawBoundBox = getBoundBoxOfDxf(dxfFile, false, 0, 0, 0);
             }
+            double rawCenterX = (renderStruct.currentRawBoundBox.bottomRightX + renderStruct.currentRawBoundBox.upperLeftX) / 2;
+            double rawCenterY = (renderStruct.currentRawBoundBox.bottomRightY + renderStruct.currentRawBoundBox.upperLeftY) / 2;
+            double rotationAngleRad = ConvertDegreesToRadians(rotationAngleDeg);
+            // next... calculate rotation bound box 
+            renderStruct.currentRotationBoundBox = getBoundBoxOfDxf(dxfFile, true, rawCenterX, rawCenterY, rotationAngleRad);
+            //and init rendering figures
         }
         private void WinFormsDxfRenderer_Paint(object sender, PaintEventArgs e)
         {
